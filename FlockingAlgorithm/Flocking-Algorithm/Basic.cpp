be3d4acfa79d799f80718ddeae306b762862188f
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-02-15 17:25:30 -0600

    Basic update

--- FlockingAlgorithm/Flocking-Algorithm/Basic.cpp
+++ FlockingAlgorithm/Flocking-Algorithm/Basic.cpp
@@ -1,6 +1,8 @@
-#include "Basic.h"
+#include "Basic.h" // Include the header file "Basic.h"
 
-double myRand() 
+// Definition of the function myRand, which generates a random number between 0 and 1
+double myRand()
 {
-	return double(double(rand()) / RAND_MAX);
+    // Using rand() to generate a random integer between 0 and RAND_MAX, and then converting it to a double between 0 and 1
+    return double(double(rand()) / RAND_MAX);
 }
--- FlockingAlgorithm/Flocking-Algorithm/Basic.h
+++ FlockingAlgorithm/Flocking-Algorithm/Basic.h
@@ -1,88 +1,89 @@
 #ifndef BASIC_H_
 #define BASIC_H_
 
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
-#include <iomanip>
-#include <limits>
+#include <cmath> // Header for mathematical functions
+#include <cstdlib> // Header for standard library functions
+#include <iostream> // Header for input and output streams
+#include <iomanip> // Header for stream manipulators
+#include <limits> // Header for numeric limits
 
+// Declaration of a function to generate a random number between 0 and 1
+double myRand();
 
-double myRand();/* {
-	return double( double(rand()) / RAND_MAX);
-}*/
-
-
+// Namespace for mathematical tools
 namespace mathtool {
 
-  //constants
+    // Constants
+
+    // Definition of PI if not already defined
 #ifndef PI
 #define PI 3.1415926535897
 #endif
 
+    // Definition of TWOPI if not already defined
 #ifndef TWOPI
 #define TWOPI 6.2831853071794
 #endif
 
-  // Return the square of a.
-  template<typename T>
-    inline T sqr(const T& a) 
-	{
-		return a*a;
-	}
+    // Function to calculate the square of a value
+    template<typename T>
+    inline T sqr(const T& a) {
+        return a * a;
+    }
 
-  // Return sign of x
-  inline int sign(double x) 
-  {
-	  return x >=0 ? 1: -1;
-  }
+    // Function to determine the sign of a number
+    inline int sign(double x) {
+        return x >= 0 ? 1 : -1;
+    }
 
-  // Angle conversions
-  inline double degToRad(double x) 
-  {
-	  return x*PI/180;
-  }
+    // Angle conversion functions
 
-  inline double radToDeg(double x) 
-  {
-	  return x*180/PI;
-  }
+    // Function to convert degrees to radians
+    inline double degToRad(double x) {
+        return x * PI / 180;
+    }
 
-  // Degree Trig functions
-  inline double sind(double x)
-  {
-	  return std::sin(degToRad(x));
-  }
+    // Function to convert radians to degrees
+    inline double radToDeg(double x) {
+        return x * 180 / PI;
+    }
 
-  inline double cosd(double x)
-  {
-	  return std::cos(degToRad(x));
-  }
+    // Trigonometric functions for degrees
 
-  inline double tand(double x)
-  {
-	  return std::tan(degToRad(x));
-  }
+    // Function to calculate the sine of an angle in degrees
+    inline double sind(double x) {
+        return std::sin(degToRad(x));
+    }
 
-  inline double asind(double x)
-  {
-	  return radToDeg(std::asin(x));
-  }
+    // Function to calculate the cosine of an angle in degrees
+    inline double cosd(double x) {
+        return std::cos(degToRad(x));
+    }
 
-  inline double acosd(double x)
-  {
-	  return radToDeg(std::acos(x));
-  }
+    // Function to calculate the tangent of an angle in degrees
+    inline double tand(double x) {
+        return std::tan(degToRad(x));
+    }
 
-  inline double atand(double x)
-  {
-	  return radToDeg(std::atan(x));
-  }
+    // Function to calculate the arcsine in degrees
+    inline double asind(double x) {
+        return radToDeg(std::asin(x));
+    }
 
-  inline double atan2d(double x, double y)
-  {
-	  return radToDeg(std::atan2(x,y));
-  }
+    // Function to calculate the arccosine in degrees
+    inline double acosd(double x) {
+        return radToDeg(std::acos(x));
+    }
+
+    // Function to calculate the arctangent in degrees
+    inline double atand(double x) {
+        return radToDeg(std::atan(x));
+    }
+
+    // Function to calculate the arctangent (y/x) in degrees
+    inline double atan2d(double x, double y) {
+        return radToDeg(std::atan2(x, y));
+    }
 
 }
 
