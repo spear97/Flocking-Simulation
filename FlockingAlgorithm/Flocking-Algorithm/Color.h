cfc2df583652b35b11db273e4ea2d31acc6775cb
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-02-15 17:32:06 -0600

    Update Color.h

--- FlockingAlgorithm/Flocking-Algorithm/Color.h
+++ FlockingAlgorithm/Flocking-Algorithm/Color.h
@@ -1,17 +1,27 @@
-#ifndef _MYCOLOR_
+#ifndef _MYCOLOR_ // Header guard to prevent multiple inclusion of the same header file
 #define _MYCOLOR_
 
-class MyColor 
+class MyColor // Declaration of the MyColor class
 {
 public:
-	MyColor(GLfloat _r, GLfloat _g, GLfloat _b, string _name="")
-	{ r=_r; g=_g; b=_b; name =_name; 
-	}
-	void Print() 
-	{ 
-		cout << "Color " << name << "(" << r << "," << g << "," << b << ")"<< endl;
-	}
-	GLfloat r, g, b;
-	string name;
+    // Constructor to initialize MyColor object with RGB values and an optional name
+    MyColor(GLfloat _r, GLfloat _g, GLfloat _b, string _name = "")
+    {
+        r = _r; // Assign red component
+        g = _g; // Assign green component
+        b = _b; // Assign blue component
+        name = _name; // Assign name if provided
+    }
+
+    // Method to print the color components and name
+    void Print()
+    {
+        cout << "Color " << name << "(" << r << "," << g << "," << b << ")" << endl; // Print color information
+    }
+
+    // Member variables to store RGB components and name of the color
+    GLfloat r, g, b;
+    string name;
 };
-#endif
+
+#endif // End of header guard
